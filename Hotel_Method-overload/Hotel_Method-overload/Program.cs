bee5f5556ace565d686f82f0dbd8e81162be2ef9
mani15015 <manikantha.sugirayyamatha@kalpitatechnologies.com>
2022-08-05 15:56:00 +0530

    New updated code

---
+++ Hotel_Method-overload/Hotel_Method-overload/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Orders
+{
+    int no_hotel;
+    string foodmenu;
+    string userinfo;
+    string addr;
+
+    public void display(int no_hotel,string foodmenu)
+    {
+        this.no_hotel = no_hotel;
+        this.foodmenu = foodmenu;
+        Console.WriteLine("available no of hotels"+no_hotel+"\n foodmenu: "+foodmenu);
+    }
+    public void display(string userinfo,string addr,string foodmenu)
+    {
+        Console.WriteLine("--------------------------------------------Hotel-------------------------------");
+        Console.WriteLine("user details\nuser name:"+userinfo+"\nuser addr:"+addr+"\norder food: "+foodmenu);
+    }
+}
+class Hotel
+{
+    static void Main()
+    {
+        Orders orders = new Orders();
+        orders.display(5, "dosa");
+        orders.display("mani", "bengalore", "dosa");
+    }
+}
